42ec536b8b97647a02f30449564f19f5a8051efe
FraAccaBi <francescahuda@gmail.com>
2022-03-23 15:10:52 +0100

    add let variables

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//Chiedi all’utente il suo nome,
+let nome = prompt('Come ti chiami?')
+console.log(nome);
+
+//poi chiedi il suo cognome,
+let cognome = prompt('Qual è il tuo cognome?')
+console.log(cognome);
+
+//poi chiedi il suo colore preferito
+let colore_prefe = prompt('Qual è il tuo colore preferito?')
+console.log(colore_prefe);
+
+//Infine scrivi sulla pagina il risultato in questo formato : nomecognomecolorepreferito22
