25717aaec8c86a2d7e9694edd88821580a8b3575
FraAccaBi <francescahuda@gmail.com>
2022-03-23 15:36:02 +0100

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,13 @@ let colore_prefe = prompt('Qual è il tuo colore preferito?')
 console.log(colore_prefe);
 
 //Infine scrivi sulla pagina il risultato in questo formato : nomecognomecolorepreferito22
+
+let password = nome + cognome + colore_prefe;
+//log test 1
+console.log(`${password}22`); 
+//log test 2
+console.log(`${nome} ${cognome} ${colore_prefe} 22`); 
+
+document.getElementById('psw').innerHTML = password + 22
+
+
